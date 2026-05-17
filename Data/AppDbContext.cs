de21a9220080db745789fb947a84381627f3ca8d
ajsjayma-cell <ajsjayma@gmail.com>
2026-05-17 09:10:17 +0800

    Initial API setup with models and database context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MassageAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(
+        DbContextOptions<AppDbContext> options
+    ) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Service> Services { get; set; }
+
+    public DbSet<Therapist> Therapists { get; set; }
+
+    public DbSet<Reservation> Reservations { get; set; }
+}
